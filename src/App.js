f29fa8962d1c69d28f1f9e8714e3fe933fef0944
puppe1990 <matheus.puppe90@hotmail.com>
2023-02-25 21:28:53 -0300

    improve design

--- src/App.js
+++ src/App.js
@@ -1,45 +1,68 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { FaCopy } from "react-icons/fa";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { darcula } from "react-syntax-highlighter/dist/esm/styles/prism";
 import prettier from "prettier/standalone";
 import babelParser from "prettier/parser-babel";
 import { Form, Container, Row, Col } from "react-bootstrap";
+import "./CodeFormatter.css";
 
 function CodeFormatter() {
   const [code, setCode] = useState("");
   const [formattedCode, setFormattedCode] = useState("");
 
-  useEffect(() => {
+  function handleChange(event) {
+    setCode(event.target.value);
+  }
+
+  function handleCopy() {
+    alert("Code copied to clipboard!");
+  }
+
+  function handleFormat() {
     // Use Prettier to format the code and update the formattedCode state
     const formatted = prettier.format(code, {
       parser: "babel",
       plugins: [babelParser],
     });
     setFormattedCode(formatted);
-  }, [code]);
-
-  function handleChange(event) {
-    setCode(event.target.value);
   }
 
   return (
-    <Container>
-      <Row className="mt-5">
-        <Col md={{ span: 6, offset: 3 }}>
+    <Container className="code-formatter-container">
+      <Row className="justify-content-center">
+        <Col md={6}>
           <Form.Control
             as="textarea"
             value={code}
             onChange={handleChange}
             placeholder="Paste your code here"
-            style={{ height: "300px" }}
+            className="code-formatter-textarea"
           />
+          <button className="code-formatter-button" onClick={handleFormat}>
+            Format
+          </button>
         </Col>
       </Row>
-      <Row className="mt-3">
-        <Col md={{ span: 6, offset: 3 }}>
-          <pre className="bg-light p-3" style={{ whiteSpace: "pre-wrap" }}>
-            {formattedCode}
-          </pre>
-        </Col>
-      </Row>
+      {formattedCode && (
+        <Row className="mt-3 justify-content-center">
+          <Col md={6}>
+            <CopyToClipboard text={formattedCode} onCopy={handleCopy}>
+              <button className="code-formatter-button mb-3">
+                <FaCopy /> Copy to Clipboard
+              </button>
+            </CopyToClipboard>
+            <SyntaxHighlighter
+              language="javascript"
+              style={darcula}
+              className="code-formatter-syntax"
+            >
+              {formattedCode}
+            </SyntaxHighlighter>
+          </Col>
+        </Row>
+      )}
     </Container>
   );
 }
