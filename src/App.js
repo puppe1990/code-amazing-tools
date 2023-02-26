eefb9a1c6483ad536dbaf630c061351862c2ebcd
puppe1990 <matheus.puppe90@hotmail.com>
2023-02-25 21:33:11 -0300

    improve design

--- src/App.js
+++ src/App.js
@@ -1,69 +1,70 @@
-import { useState } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import { FaCopy } from "react-icons/fa";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { darcula } from "react-syntax-highlighter/dist/esm/styles/prism";
-import prettier from "prettier/standalone";
-import babelParser from "prettier/parser-babel";
-import { Form, Container, Row, Col } from "react-bootstrap";
-import "./CodeFormatter.css";
+import { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { FaCopy } from 'react-icons/fa';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import prettier from 'prettier/standalone';
+import babelParser from 'prettier/parser-babel';
+import { Form, Container, Row, Col, Navbar } from 'react-bootstrap';
+import './CodeFormatter.css';
 
 function CodeFormatter() {
-  const [code, setCode] = useState("");
-  const [formattedCode, setFormattedCode] = useState("");
+  const [code, setCode] = useState('');
+  const [formattedCode, setFormattedCode] = useState('');
 
   function handleChange(event) {
     setCode(event.target.value);
   }
 
   function handleCopy() {
-    alert("Code copied to clipboard!");
+    alert('Code copied to clipboard!');
   }
 
   function handleFormat() {
     // Use Prettier to format the code and update the formattedCode state
     const formatted = prettier.format(code, {
-      parser: "babel",
+      parser: 'babel',
       plugins: [babelParser],
     });
     setFormattedCode(formatted);
   }
 
   return (
-    <Container className="code-formatter-container">
-      <Row className="justify-content-center">
-        <Col md={6}>
-          <Form.Control
-            as="textarea"
-            value={code}
-            onChange={handleChange}
-            placeholder="Paste your code here"
-            className="code-formatter-textarea"
-          />
-          <button className="code-formatter-button" onClick={handleFormat}>
-            Format
-          </button>
-        </Col>
-      </Row>
-      {formattedCode && (
-        <Row className="mt-3 justify-content-center">
-          <Col md={6}>
-            <CopyToClipboard text={formattedCode} onCopy={handleCopy}>
-              <button className="code-formatter-button mb-3">
-                <FaCopy /> Copy to Clipboard
-              </button>
-            </CopyToClipboard>
-            <SyntaxHighlighter
-              language="javascript"
-              style={darcula}
-              className="code-formatter-syntax"
-            >
-              {formattedCode}
-            </SyntaxHighlighter>
+    <div className="code-formatter-container">
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>Code Formatter</Navbar.Brand>
+      </Navbar>
+      <Container>
+        <Row className="justify-content-center">
+          <Col md={8}>
+            <Form.Control
+              as="textarea"
+              value={code}
+              onChange={handleChange}
+              placeholder="Paste your code here"
+              className="code-formatter-textarea"
+            />
+            <button className="code-formatter-button" onClick={handleFormat}>
+              Format
+            </button>
           </Col>
         </Row>
-      )}
-    </Container>
+        {formattedCode && (
+          <Row className="mt-3 justify-content-center">
+            <Col md={8}>
+              <CopyToClipboard text={formattedCode} onCopy={handleCopy}>
+                <button className="code-formatter-button mb-3">
+                  <FaCopy /> Copy to Clipboard
+                </button>
+              </CopyToClipboard>
+              <SyntaxHighlighter language="javascript" style={darcula} className="code-formatter-syntax">
+                {formattedCode}
+              </SyntaxHighlighter>
+            </Col>
+          </Row>
+        )}
+      </Container>
+    </div>
   );
 }
 
