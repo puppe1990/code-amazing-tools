2c13c4a5a356fe6045f0e6b432c765380d013d96
puppe1990 <matheus.puppe90@hotmail.com>
2023-02-25 21:16:22 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import prettier from "prettier/standalone";
+import babelParser from "prettier/parser-babel";
+import { Form, Container, Row, Col } from "react-bootstrap";
+
+function CodeFormatter() {
+  const [code, setCode] = useState("");
+  const [formattedCode, setFormattedCode] = useState("");
+
+  useEffect(() => {
+    // Use Prettier to format the code and update the formattedCode state
+    const formatted = prettier.format(code, {
+      parser: "babel",
+      plugins: [babelParser],
+    });
+    setFormattedCode(formatted);
+  }, [code]);
+
+  function handleChange(event) {
+    setCode(event.target.value);
+  }
+
+  return (
+    <Container>
+      <Row className="mt-5">
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form.Control
+            as="textarea"
+            value={code}
+            onChange={handleChange}
+            placeholder="Paste your code here"
+            style={{ height: "300px" }}
+          />
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col md={{ span: 6, offset: 3 }}>
+          <pre className="bg-light p-3" style={{ whiteSpace: "pre-wrap" }}>
+            {formattedCode}
+          </pre>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CodeFormatter />
     </div>
   );
 }
